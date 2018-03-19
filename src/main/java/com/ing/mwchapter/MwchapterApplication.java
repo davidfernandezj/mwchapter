70ee920bbfd219bf1ea31afaa6c93c2fd33e956c
David Fernandez <david.fernandez1987@gmail.com>
2018-03-19 23:20:10 +0100

    First commit

---
+++ src/main/java/com/ing/mwchapter/MwchapterApplication.java
@@ -0,0 +1,12 @@
+package com.ing.mwchapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MwchapterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MwchapterApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ing/mwchapter/MwchapterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ing.mwchapter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MwchapterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
